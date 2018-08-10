abb4810a3f1b1ec02b59af115231ff826e50d451
Ronja <Ronja.Koehling@viadee.de>
2018-08-10 15:43:18 +0200

    Holidays in getrennten R-file

--- R/viadeeTheme.R
+++ R/viadeeTheme.R
@@ -1,9 +1,7 @@
 library(ggplot2)
 library(grid)
 library(ggthemes)
-library(httr)
-library(jsonlite)
-library(lubridate)
+
 
 
 #' A function that styles a ggplot
@@ -81,27 +79,4 @@ viadeePlot_purple<-function(graph)
   return (viadeePlot(graph, c("#7EBCA9","#E5344E")))
 }
 
-#' Return True when the given date is a holiday in the given state in Germany
-#'
-#' uses the https://feiertage-api.de/ api. Check the website for an overview of the
-#' state keys
-#'
-#'
-#' @param day a date or String in the format yyyy-mm-dd
-#' @param state a key for the state e.g. NI for Niedersachsen
-#' @export
-#' @examples
-#' is_holiday("2018-12-25", "NI")
-is_holiday <- function(day, state){
-  day <- as.character(day)
-
-  year <- lubridate::year(ymd(day))
-  url <- paste("https://feiertage-api.de/api/?jahr=",year,"&nur_land=",state, sep = "")
-  feiertage <- GET(url)
-  feiertage_text <- content(feiertage, "text", encoding = "UTF-8")
-  feiertage_json <- unlist(fromJSON(feiertage_text, flatten = TRUE), use.names = FALSE)
-
-  return(day %in% feiertage_json)
-
-}
 
