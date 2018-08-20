bee63cb1e7af2901684a295ad581e433fbe8cead
Anton Laukemper <anton.laukemper@viadee.de>
2018-08-20 15:13:21 +0200

    comments added

--- R/Holiday.R
+++ R/Holiday.R
@@ -14,14 +14,15 @@ library(lubridate)
 #' @examples
 #' is_holiday("2018-12-25", "NI")
 is_holiday <- function(day, state){
+  #der Tag wird in String konvertiert wird, da später ein String in einer Liste gesucht wird
   day <- as.character(day)
-
   year <- lubridate::year(ymd(day))
   url <- paste("https://feiertage-api.de/api/?jahr=",year,"&nur_land=",state, sep = "")
   feiertage <- GET(url)
   feiertage_text <- content(feiertage, "text", encoding = "UTF-8")
+  #das JSON Element wird in eine Liste umgewandelt, damit in der Liste gesucht werden kann
   feiertage_json <- unlist(fromJSON(feiertage_text, flatten = TRUE), use.names = FALSE)
-
+  
   return(day %in% feiertage_json)
 
 }
